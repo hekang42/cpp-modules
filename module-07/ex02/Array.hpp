407af587c7150097f367664408f94d2c9515ecfe
hekang <glipung@naver.com>
2021-09-24 16:26:12 +0900

    end module 07

--- module-07/ex02/Array.hpp
+++ module-07/ex02/Array.hpp
@@ -5,10 +5,63 @@ template <typename T>
 class Array
 {
 private:
-
+	unsigned int _size;
+	T *array;
 public:
-	Array();
-	Array(unsigned int n);
+	Array()
+	{
+		_size = 0;
+		array = new T[0];
+	}
+	Array(unsigned int n)
+	{
+		_size = n;
+		array = new T[n];
+	}
+	Array(const Array &other)
+	{
+		this->array = new T[other.size()];
+		this->_size = other.size();
+		for (unsigned int i = 0; i < other.size(); i++)
+			this->array[i] = other[i];
+	}
+	Array<T> &operator=(const Array &other)
+	{
+		this->~Array();
+		this->array = new T[other.size()];
+		this->_size = other.size();
+		for (unsigned int i = 0; i < other.size(); i++)
+			this->array[i] = other[i];
+		return *this;
+	}
+	T &operator[](int i) const
+	{
+		if (i < 0 || i >= static_cast<int>(this->size()))
+			throw Array::OutOfRangeException();
+		return this->array[i];
+	}
+	T &operator[](int i)
+	{
+		if (i < 0 || i >= static_cast<int>(this->size()))
+			throw Array::OutOfRangeException();
+		return this->array[i];
+	}
+	~Array()
+	{
+		delete[] this->array;
+	}
+	unsigned int size() const
+	{
+		return this->_size;
+	}
+
+	class OutOfRangeException : public std::exception
+	{
+		virtual const char *what() const throw()
+		{
+			return "Out of range";
+		}
+	};
 };
 
 #endif
